b5aab4a83b1ff6a530befd84bf4aec29f084c262
Austin Roach <ahroach@gmail.com>
2012-04-04 18:21:22 -0400

    Added linear algebra stuff for zggev
    In principle this should work now. But it can't be tested without further
    changes to my wrappers and everything.

--- eigensolve_full.c
+++ eigensolve_full.c
@@ -11,6 +11,10 @@
 #include <gsl/gsl_sf_bessel.h>
 #include "global.h"
 
+void wAelbg(int i, int j, COMPRESSED_MATRIX *matrix, double complex value);
+void wBelbg(int i, int j, COMPRESSED_MATRIX *matrix, double complex value);
+
+
 RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix, 
 				  PARAMS_STRUCT *params, GRID_STRUCT *grid,
 				  ROTATION_STRUCT *rotation,
@@ -27,109 +31,142 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
   double Im, Imp1, Km, Kmp1;
   double scalefactorpi, scalefactorv, scalefactorb, tempscalefactor;
 
-  //Zero out all of the elements of A.
-  for (int i=0; i < matrix->n*matrix->lda; i++) {
+  //Variables needed to run the LAPACK routine
+  int info;
+  int lwork;
+  double complex *lambda;
+  double complex *alpha;
+  double complex *beta;
+  double complex *vr;
+  double complex *work;
+  double complex *rwork;
+  int ldvl;
+  int ldvr;
+
+  char jobvl;
+  char jobvr;
+
+  //Since we're not using the compressed matrix here, we need to reallocate
+  //some more space
+  free(matrix->A);
+  free(matrix->B);
+
+  matrix->A = malloc(sizeof(double complex)*matrix->n*matrix->n);
+  if (matrix->A == NULL) {
+    fprintf(stderr, "Unable to allocate memory for A.\n");
+    exit(1);
+  }
+
+  matrix->B = malloc(sizeof(double complex)*matrix->n*matrix->n);
+  if (matrix->A == NULL) {
+    fprintf(stderr, "Unable to allocate memory for B.\n");
+    exit(1);
+  }
+
+  //Zero out all of the elements of A and B.
+  for (int i=0; i < matrix->n*matrix->n; i++) {
     matrix->A[i]=0.0;
+    matrix->B[i]=0.0;
   }
 
   // First we'll fill out the matrix for the beta_r terms in the fluid
   for (int i=grid->is; i <= grid->ie; i++) {
     // beta_r_i^(n+1) terms
-    wAelem(5*(i) + 0, 5*(i) + 0, matrix,
+    wAelbg(5*(i) + 0, 5*(i) + 0, matrix,
 	   -eta*grid->diffuse[i] 
 	   - I*params->m*rotation->omega[i]);
     
 
     // beta_r_(i+1)^(n+1) terms
-    wAelem(5*(i) + 0, 5*(i+1) + 0, matrix, eta*grid->diffuse2[i]);
+    wAelbg(5*(i) + 0, 5*(i+1) + 0, matrix, eta*grid->diffuse2[i]);
 
     // beta_r_(i-1)^(n+1) terms
-    wAelem(5*(i) + 0, 5*(i-1) + 0, matrix, eta*grid->diffuse2[i]);
+    wAelbg(5*(i) + 0, 5*(i-1) + 0, matrix, eta*grid->diffuse2[i]);
     // beta_theta_i^(n+1) terms
-    wAelem(5*(i) + 0, 5*(i) + 1, matrix, -I*2*eta*params->m
+    wAelbg(5*(i) + 0, 5*(i) + 1, matrix, -I*2*eta*params->m
 	  *grid->r2inv[i]);
 
     // phi_r_i^(n+1) terms
-    wAelem(5*(i) + 0, 5*(i) + 2, matrix, kva);
+    wAelbg(5*(i) + 0, 5*(i) + 2, matrix, kva);
   }
 
   // Next the beta_theta terms
   for (int i=grid->is; i <= grid->ie; i++) {
     // beta_theta_i^(n+1) terms
-    wAelem(5*(i) + 1, 5*(i) + 1, matrix,
+    wAelbg(5*(i) + 1, 5*(i) + 1, matrix,
 	   -eta*grid->diffuse[i] 
 	   - I*params->m*rotation->omega[i]);
 
     // beta_theta_(i+1)^(n+1) terms
-    wAelem(5*(i) + 1, 5*(i+1) + 1, matrix, eta*grid->diffuse2[i]);
+    wAelbg(5*(i) + 1, 5*(i+1) + 1, matrix, eta*grid->diffuse2[i]);
     
     // beta_theta_(i-1)^(n+1) terms
-    wAelem(5*(i) + 1, 5*(i-1) + 1, matrix, eta*grid->diffuse2[i]);
+    wAelbg(5*(i) + 1, 5*(i-1) + 1, matrix, eta*grid->diffuse2[i]);
 
     //beta_r_i^(n+1) terms
-    wAelem(5*(i) + 1, 5*(i) + 0, matrix,
+    wAelbg(5*(i) + 1, 5*(i) + 0, matrix,
 	   0.5*(rotation->omega[i+1] -
 		       rotation->omega[i-1])/grid->dx
 	   + I*2.0*eta*params->m*grid->r2inv[i]);
 
     //phi_theta_i^(n+1) terms
-    wAelem(5*(i) + 1, 5*(i) + 3, matrix, kva);
+    wAelbg(5*(i) + 1, 5*(i) + 3, matrix, kva);
   }
 
   // Next the phi_r terms
   for (int i=grid->is; i <= grid->ie; i++) {
     // phi_r_i^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i) + 2, matrix,
+    wAelbg(5*(i) + 2, 5*(i) + 2, matrix,
 	   -nu*grid->diffuse[i] - I*params->m*rotation->omega[i]);
 
     // phi_r_(i+1)^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i+1) + 2, matrix, nu*grid->diffuse2[i]);
+    wAelbg(5*(i) + 2, 5*(i+1) + 2, matrix, nu*grid->diffuse2[i]);
 
     //phi_r_(i-1)^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i-1) + 2, matrix, nu*grid->diffuse2[i]);
+    wAelbg(5*(i) + 2, 5*(i-1) + 2, matrix, nu*grid->diffuse2[i]);
 
     //beta_r_i^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i) + 0, matrix, -kva);
+    wAelbg(5*(i) + 2, 5*(i) + 0, matrix, -kva);
     
     //phi_theta_i^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i) + 3, matrix, 2.0*
+    wAelbg(5*(i) + 2, 5*(i) + 3, matrix, 2.0*
 	  rotation->omega[i]
 	  - I*2.0*nu*params->m*grid->r2inv[i]);
 
     //pi_(i+1)^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i+1) + 4, matrix, -0.5/
+    wAelbg(5*(i) + 2, 5*(i+1) + 4, matrix, -0.5/
 	   (grid->dx*grid->r[i]));
 
     //pi_(i-1)^(n+1) terms
-    wAelem(5*(i) + 2, 5*(i-1) + 4, matrix, 0.5/
+    wAelbg(5*(i) + 2, 5*(i-1) + 4, matrix, 0.5/
 	   (grid->dx*grid->r[i]));
   }
 
   // Next the phi_theta terms
   for (int i=grid->is; i <= grid->ie; i++) {
     // phi_theta_i^(n+1) terms
-    wAelem(5*(i) + 3, 5*(i) + 3, matrix,
+    wAelbg(5*(i) + 3, 5*(i) + 3, matrix,
 	  -nu*grid->diffuse[i] 
 	  - I*params->m*rotation->omega[i]);
 
     // phi_theta_(i+1)^(n+1) terms
-    wAelem(5*(i) + 3, 5*(i+1) + 3, matrix, nu*grid->diffuse2[i]);
+    wAelbg(5*(i) + 3, 5*(i+1) + 3, matrix, nu*grid->diffuse2[i]);
 
     //phi_theta_(i-1)^(n+1) terms
-    wAelem(5*(i) + 3, 5*(i-1) + 3, matrix, nu*grid->diffuse2[i]);
+    wAelbg(5*(i) + 3, 5*(i-1) + 3, matrix, nu*grid->diffuse2[i]);
 
     //beta_theta_i^(n+1) terms
-    wAelem(5*(i) + 3, 5*(i) + 1, matrix, -kva);
+    wAelbg(5*(i) + 3, 5*(i) + 1, matrix, -kva);
 
     //phi_r_i^(n+1) terms
-    wAelem(5*(i) + 3, 5*(i) + 2, matrix,
+    wAelbg(5*(i) + 3, 5*(i) + 2, matrix,
 	   -2.0*rotation->omega[i] 
 	   - 0.5*(1/grid->dx)*
 	   (rotation->omega[i+1]-rotation->omega[i-1])
 	   + I*2.0*nu*params->m*grid->r2inv[i]);
 
     //pi_i^(n+1) terms
-    wAelem(5*(i) + 3, 5*(i) + 4, matrix, -I*params->m/grid->r[i]);
+    wAelbg(5*(i) + 3, 5*(i) + 4, matrix, -I*params->m/grid->r[i]);
   }
 
   // Next the pi terms
@@ -138,34 +175,34 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
     scalefactorpi = nu*grid->diffuse[(grid->ie-grid->is)/2]/
       (2.0*dx2inv*grid->r2inv[i] + m2*grid->r2inv[i] + k2);
     //pi_i^(n+1) terms
-    wAelem(5*(i) + 4, 5*(i) + 4, matrix,
+    wAelbg(5*(i) + 4, 5*(i) + 4, matrix,
            -(2.0*dx2inv*grid->r2inv[i] + m2*grid->r2inv[i] + k2)
 	   *scalefactorpi);
 
     //pi_(i+1)^(n+1) terms
-    wAelem(5*(i) + 4, 5*(i+1) + 4, matrix, dx2inv*grid->r2inv[i]
+    wAelbg(5*(i) + 4, 5*(i+1) + 4, matrix, dx2inv*grid->r2inv[i]
 	   *scalefactorpi);
 
     //pi_(i-1)^(n+1) terms
-    wAelem(5*(i) + 4, 5*(i-1) + 4, matrix, dx2inv*grid->r2inv[i]
+    wAelbg(5*(i) + 4, 5*(i-1) + 4, matrix, dx2inv*grid->r2inv[i]
 	   *scalefactorpi);
 
     //phi_r_i^(n+1) terms
-    wAelem(5*(i) + 4, 5*(i) + 2, matrix,
+    wAelbg(5*(i) + 4, 5*(i) + 2, matrix,
 	   (2.0*I*params->m/grid->r[i])*
 	   (rotation->omega[i] + (0.5/grid->dx)*
 	    (rotation->omega[i+1] - rotation->omega[i-1]))*scalefactorpi);
     
     //phi_theta_i^(n+1) terms
-    wAelem(5*(i) + 4, 5*(i) + 3, matrix,
+    wAelbg(5*(i) + 4, 5*(i) + 3, matrix,
     	   (-2.0*rotation->omega[i]/grid->r[i])*scalefactorpi);
 
     //phi_theta_(i+1)^(n+1) terms
-    wAelem(5*(i) + 4, 5*(i+1) + 3, matrix, 
+    wAelbg(5*(i) + 4, 5*(i+1) + 3, matrix, 
 	   (-rotation->omega[i+1]/(grid->r[i]*grid->dx))*scalefactorpi);
 
     //phi_theta_(i-1)^(n+2) terms
-    wAelem(5*(i) + 4, 5*(i-1) + 3, matrix,
+    wAelbg(5*(i) + 4, 5*(i-1) + 3, matrix,
 	   (rotation->omega[i-1]/(grid->r[i]*grid->dx))*scalefactorpi);    
   }
 
@@ -182,12 +219,12 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
     // Perfectly-conducting boundary condition for beta_r */
     
     //beta_r_0 + beta_r_1 = 0
-    wAelem(5*(grid->is-1) + 0, 5*(grid->is) + 0, matrix, scalefactorb);
-    wAelem(5*(grid->is-1) + 0, 5*(grid->is-1) + 0, matrix, scalefactorb);
+    wAelbg(5*(grid->is-1) + 0, 5*(grid->is) + 0, matrix, scalefactorb);
+    wAelbg(5*(grid->is-1) + 0, 5*(grid->is-1) + 0, matrix, scalefactorb);
 
     //beta_r_N+1 + beta_r_N = 0
-    wAelem(5*(grid->ie+1), 5*(grid->ie+1), matrix, scalefactorb);
-    wAelem(5*(grid->ie+1), 5*(grid->ie), matrix, scalefactorb);
+    wAelbg(5*(grid->ie+1), 5*(grid->ie+1), matrix, scalefactorb);
+    wAelbg(5*(grid->ie+1), 5*(grid->ie), matrix, scalefactorb);
 	   
     // Perfectly-conducting boundary condition for beta_theta
     
@@ -195,25 +232,25 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
     facout = (1-0.5*grid->dx)/(1+0.5*grid->dx);
 
     //beta_theta_0 - facin*beta_theta_1 = 0
-    wAelem(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix, 
+    wAelbg(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix, 
 	   -facin*scalefactorb);
-    wAelem(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, scalefactorb);
+    wAelbg(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, scalefactorb);
 
     //beta_theta_N+1 - facout*beta_theta_N = 0
-    wAelem(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix,
+    wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix,
 	   scalefactorb);
-    wAelem(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix,
+    wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix,
 	   -facout*scalefactorb);
 
   } else if(params->magnetic_bc == 1) {
     /*
     //beta_theta = 0 at inner boundary
-    wAelem(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, scalefactorb);
-    wAelem(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix, scalefactorb);
+    wAelbg(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, scalefactorb);
+    wAelbg(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix, scalefactorb);
 
     //beta_theta = 0 at outer boundary
-    wAelem(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix, scalefactorb);
-    wAelem(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix, scalefactorb);
+    wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix, scalefactorb);
+    wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix, scalefactorb);
 
     //Condition on beta_r
     Im = gsl_sf_bessel_In(params->m, params->k*params->r1);
@@ -232,16 +269,16 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
 
     tempscalefactor = scalefactorb/facin;
 
-    wAelem(5*(grid->is-1) + 0, 5*(grid->is-1) + 0, matrix, 
+    wAelbg(5*(grid->is-1) + 0, 5*(grid->is-1) + 0, matrix, 
 	   tempscalefactor*(facin + 1.0));
-    wAelem(5*(grid->is-1) + 0, 5*(grid->is) + 0, matrix,
+    wAelbg(5*(grid->is-1) + 0, 5*(grid->is) + 0, matrix,
 	   tempscalefactor*(facin - 1.0));
     
     tempscalefactor = scalefactorb/facout;
 
-    wAelem(5*(grid->ie+1) + 0, 5*(grid->ie+1) + 0, matrix,
+    wAelbg(5*(grid->ie+1) + 0, 5*(grid->ie+1) + 0, matrix,
 	   tempscalefactor*(facout - 1.0));
-    wAelem(5*(grid->ie+1) + 0, 5*(grid->ie) + 0, matrix,
+    wAelbg(5*(grid->ie+1) + 0, 5*(grid->ie) + 0, matrix,
 	   tempscalefactor*(facout + 1.0));
     */
 
@@ -261,25 +298,25 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
       + (params->m/(params->k*grid->r[grid->ie]))*
       gsl_sf_bessel_Kn((params->m), params->k*grid->r[grid->ie]);
 
-    wAelem(5*(grid->is-1) + 0, 5*(grid->is-1) + 0, matrix, 
+    wAelbg(5*(grid->is-1) + 0, 5*(grid->is-1) + 0, matrix, 
 	   1.0);
-    wAelem(5*(grid->is-1) + 0, 5*(grid->is) + 0, matrix,
+    wAelbg(5*(grid->is-1) + 0, 5*(grid->is) + 0, matrix,
 	   -constin1/constin2);
     
-    wAelem(5*(grid->ie+1) + 0, 5*(grid->ie+1) + 0, matrix,
+    wAelbg(5*(grid->ie+1) + 0, 5*(grid->ie+1) + 0, matrix,
 	   1.0);
-    wAelem(5*(grid->ie+1) + 0, 5*(grid->ie) + 0, matrix,
+    wAelbg(5*(grid->ie+1) + 0, 5*(grid->ie) + 0, matrix,
 	   -constout1/constout2);
 
     //beta_theta boundary condition
     if(params->m == 0) {
       //beta_theta = 0 at inner boundary
-      wAelem(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, 1.0);
-      wAelem(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix, 1.0);
+      wAelbg(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, 1.0);
+      wAelbg(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix, 1.0);
       
       //beta_theta = 0 at outer boundary
-      wAelem(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix, 1.0);
-      wAelem(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix, 1.0);
+      wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix, 1.0);
+      wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix, 1.0);
     } else {
       //match to insulating solution
       constin1 = gsl_sf_bessel_In((params->m), params->k*grid->r[grid->is-1])
@@ -291,12 +328,12 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
       constout2 = gsl_sf_bessel_Kn((params->m), params->k*grid->r[grid->ie])
 	*grid->r[grid->ie+1];
 
-      wAelem(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, 1.0);
-      wAelem(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix,
+      wAelbg(5*(grid->is-1) + 1, 5*(grid->is-1) + 1, matrix, 1.0);
+      wAelbg(5*(grid->is-1) + 1, 5*(grid->is) + 1, matrix,
 	     -constin1/constin2);
       
-      wAelem(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix, 1.0);
-      wAelem(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix,
+      wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie+1) + 1, matrix, 1.0);
+      wAelbg(5*(grid->ie+1) + 1, 5*(grid->ie) + 1, matrix,
 	     -constout1/constout2);
     }
 
@@ -305,47 +342,110 @@ RESULTS_STRUCT *eigensolve_direct(COMPRESSED_MATRIX *matrix,
   // No-slip boundary condition for phi_r, phi_theta  
 
   //phi_theta = 0 at boundary by setting phi_theta_0 + phi_theta_1 = 0
-  wAelem(5*(grid->is-1) + 3, 5*(grid->is-1) + 3, matrix, scalefactorv);
-  wAelem(5*(grid->is-1) + 3, 5*(grid->is) + 3, matrix, scalefactorv);
+  wAelbg(5*(grid->is-1) + 3, 5*(grid->is-1) + 3, matrix, scalefactorv);
+  wAelbg(5*(grid->is-1) + 3, 5*(grid->is) + 3, matrix, scalefactorv);
 
   //phi_theta_N+1=0, phi_theta_N = 0
-  wAelem(5*(grid->ie+1) + 3, 5*(grid->ie+1) + 3, matrix, scalefactorv);
-  wAelem(5*(grid->ie+1) + 3, 5*(grid->ie) + 3, matrix, scalefactorv);
+  wAelbg(5*(grid->ie+1) + 3, 5*(grid->ie+1) + 3, matrix, scalefactorv);
+  wAelbg(5*(grid->ie+1) + 3, 5*(grid->ie) + 3, matrix, scalefactorv);
 
 
   //phi_r = 0 = dphi_r/dr at boundary. Set points on both sides of boundary
   //to zero
-  wAelem(5*(grid->is-1) + 2, 5*(grid->is-1) + 2, matrix, scalefactorv);
+  wAelbg(5*(grid->is-1) + 2, 5*(grid->is-1) + 2, matrix, scalefactorv);
   //Note that this equation is actually a constraint on dphi_r just inside
   //the boundary.  It is *not* an equation for Pi.
-  wAelem(5*(grid->is-1) + 4, 5*(grid->is) + 2, matrix, scalefactorv);
+  wAelbg(5*(grid->is-1) + 4, 5*(grid->is) + 2, matrix, scalefactorv);
   
-  wAelem(5*(grid->ie+1) + 2, 5*(grid->ie+1) + 2, matrix, scalefactorv);
-  wAelem(5*(grid->ie+1) + 4, 5*(grid->ie) + 2, matrix, scalefactorv);
-
-
-
-  //Zero out all of the elements of B and Bb.
-  for (int i=0; i < matrix->n*matrix->lda; i++) {
-    matrix->B[i]=0.0;
-  }
+  wAelbg(5*(grid->ie+1) + 2, 5*(grid->ie+1) + 2, matrix, scalefactorv);
+  wAelbg(5*(grid->ie+1) + 4, 5*(grid->ie) + 2, matrix, scalefactorv);
 
 
   //Now write 1s on the diagonal for the non-constraint equations
   for (int i=grid->is; i <= grid->ie; i++) {  
-    wBelem((5*(i) + 0) , (5*(i) + 0), matrix, 1.0);
-    wBelem((5*(i) + 1) , (5*(i) + 1), matrix, 1.0);
-    wBelem((5*(i) + 2) , (5*(i) + 2), matrix, 1.0);
-    wBelem((5*(i) + 3) , (5*(i) + 3), matrix, 1.0);
+    wBelbg((5*(i) + 0) , (5*(i) + 0), matrix, 1.0);
+    wBelbg((5*(i) + 1) , (5*(i) + 1), matrix, 1.0);
+    wBelbg((5*(i) + 2) , (5*(i) + 2), matrix, 1.0);
+    wBelbg((5*(i) + 3) , (5*(i) + 3), matrix, 1.0);
   }
 
+  //Now allocate space for all of the LAPACK variables
 
   results = malloc(sizeof(RESULTS_STRUCT));
-  results->lambda = malloc(sizeof(double complex)
-			   *(arpack_params->nummodes+1));
-  results->z = malloc(sizeof(double complex)
-		      *arpack_params->nummodes*matrix->n);
-  results->residual = malloc(sizeof(double)*(arpack_params->nummodes + 1));
+  assert(results);
+  results->lambda = malloc(sizeof(double complex)*matrix->n);
+  assert(results->lambda);
+  results->z = malloc(sizeof(double complex)*matrix->n*matrix->n);
+  assert(results->z);
+  results->residual = malloc(sizeof(double)*matrix->n);
+  assert(results->residual);
 
+  alpha = malloc(sizeof(double complex)*matrix->n);
+  assert(alpha);
+  beta = malloc(sizeof(double complex)*matrix->n);
+  assert(beta);
+  work = malloc(sizeof(double complex)*8*matrix->n);
+  assert(work);
+  rwork = malloc(sizeof(double)*8*matrix->n);
+  assert(rwork);
+  
+  jobvl = 'N';
+  jobvr = 'V';
+  ldvl = 1;
+  ldvr = matrix->n;
+  lwork = -1;
+
+  //Run the routine once to have it return the optimum size of the work array
+  zggev_(&jobvl, &jobvr, &matrix->n, matrix->A, &matrix->n, matrix->B,
+	 &matrix->n, alpha, beta, NULL, &ldvl, results->z, &ldvr, work,
+	 &lwork, rwork, &info);
+
+  lwork = creal(work[0]);
+  free(work);
+  work = malloc(sizeof(double complex)*lwork);
+  assert(work);
+
+  //Now run again to actually solve the eigenproblem.
+  zggev_(&jobvl, &jobvr, &matrix->n, matrix->A, &matrix->n, matrix->B,
+	 &matrix->n, alpha, beta, NULL, &ldvl, results->z, &ldvr, work,
+	 &lwork, rwork, &info);
+  
+  if (info != 0) {
+    printf("Error: ZGGEV returned %i.\n", info);
+  }
+
+  //Now store the eigenvalues from the calculation
+  for (int i = 0; i < matrix->n; i++) {
+    results->lambda[i] = alpha[i]/beta[i];
+  }
+
+  //And lets get out of here!
+  free(alpha);
+  free(beta);
+  free(vr);
+  free(work);
+  free(rwork);
   return results;
+}
+
+
+void wAelbg(int i, int j, COMPRESSED_MATRIX *matrix, double complex value) {
+  //We are writing to the FULL SIZED UNCOMPRESSED MATRIX here.
+  //We simply find the position of the elemtn in the *column-major* matrix
+  //structure, and write the value.
+
+  int k;
+  k  matrix->n*j + i;
+  matrix->A[k] = value;
+}
+
+
+void wBelbg(int i, int j, COMPRESSED_MATRIX *matrix, double complex value) {
+  //We are writing to the FULL SIZED UNCOMPRESSED MATRIX here.
+  //We simply find the position of the elemtn in the *column-major* matrix
+  //structure, and write the value.
+
+  int k;
+  k  matrix->n*j + i;
+  matrix->B[k] = value;
 }
