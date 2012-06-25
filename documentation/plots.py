0236d41b80da3b8c59a354b6d982e056d7b3ffb0
Austin Roach <ahroach@gmail.com>
2012-06-25 08:06:54 -0400

    Add code for generating plots used in documentation

---
+++ documentation/plots.py
@@ -0,0 +1,167 @@
+import sys
+sys.path.append("/home/MRI/globalcode/python")
+import readnetcdf
+import matplotlib.pyplot as pyplot
+import numpy
+import scipy.io.netcdf as netcdf
+
+
+def plot_growth_rate_convergence(filename="growth_rate_convergence.eps"):
+    datapath = "/home/MRI/globalcode/benchmarks_v2/goodman_mri_m0_arpack_resscan/"
+    grs = []
+    n = []
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_100.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(100)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_200.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(200)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_400.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(400)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_500.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(500)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_600.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(600)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_800.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(800)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_1000.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(1000)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_2000.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(2000)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_4000.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(4000)
+    ncfile.close()
+
+    ncfile = netcdf.netcdf_file(datapath + "m0_8000.nc", 'r')
+    grs.append(ncfile.variables['lambda'][0][0])
+    n.append(8000)
+    ncfile.close()
+
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(n, grs, '*-')
+    ax.semilogx()
+    ax.set_xlabel("Number of grid cells")
+    ax.set_ylabel("Growth rate")
+
+    fig.savefig(filename, bbox_inches='tight', pad_inches=0.01)
+
+def plot_timings(filename="timings.eps"):
+    numcells = [50, 100, 200, 400, 800,
+                1600, 3200, 6400, 9600]
+    t_full = [0.560, 4.080, 34.50, 295.331, 3519.7, 
+              37061, 281511, numpy.nan, numpy.nan]
+    m_full = [102, 110, 161, 350, 1102,
+              4106, 16114, numpy.nan, numpy.nan]
+    t_10 = [0.117, 3.410, 3.327, 2.733, 5.357,
+            8.967, 60, 156, 264]
+    m_10 = [numpy.nan, 99, 100, 101, 106,
+            115, 132, 166, 200]
+    t_100 = [numpy.nan, 3.957, 4.567, 4.784, 8.886,
+             15.621, 68.77, 155.2, 265]
+    m_100 = [numpy.nan, 99, 107, 114, 125,
+             150, 183, 318, 428]
+    t_1000 = [numpy.nan, numpy.nan, numpy.nan, numpy.nan, 473.7,
+              646.4, 987.8, 1628, 2426]
+    m_1000 = [numpy.nan, numpy.nan, numpy.nan, numpy.nan, 590,
+              674, 1071, 1856, 2641]
+
+    fig = pyplot.figure()
+    ax1 = fig.add_subplot(2,1,1)
+    ax1.plot(numcells, t_full, "b*-", label="Full solution")
+    ax1.plot(numcells, t_10, "rd-", label="ARPACK, 10 eigenvalues")
+    ax1.plot(numcells, t_100, "gs-", label="ARPACK, 100 eigenvalues")
+    ax1.plot(numcells, t_1000, "kp-", label="ARPACK, 1000 eigenvalues")
+    ax1.set_xlim(30,10000)
+    ax1.loglog()
+    ax1.set_ylabel("Run time [s]")
+
+    ax2 = fig.add_subplot(2,1,2)
+    ax2.plot(numcells, m_full, "b*-", label="Full solution")
+    ax2.plot(numcells, m_10, "rd-", label="ARPACK, 10 values")
+    ax2.plot(numcells, m_100, "gs-", label="ARPACK, 100 values")
+    ax2.plot(numcells, m_1000, "kp-", label="ARPACK, 1000 values")
+    ax2.set_xlim(30,10000)
+    ax2.loglog()
+    ax2.set_ylabel("Memory used [MB]")
+    ax2.set_xlabel("Number of grid cells")
+    ax2.legend(loc='upper left')
+    
+    ax1.axhline(60, color='k')
+    ax1.text(35, 60*1.15, "Trip to water cooler")
+
+    ax1.axhline(1800, color='k')
+    ax1.text(35, 1800*1.10, "Lunch break")
+
+    ax1.axhline(28800, color='k')
+    ax1.text(35, 28800*1.15, "Overnight")
+
+    fig.savefig(filename, bbox_inches='tight', pad_inches=0.01)
+
+def plot_goodman_mri_insulating_mode(filename="goodman_mri_insulating_mode.eps"):
+    datapath="/home/MRI/globalcode/benchmarks_v2/goodman_mri/"
+
+    ncfile = netcdf.netcdf_file(datapath + "m0.nc", 'r')
+
+    r = ncfile.variables['r'][:]
+    
+    vr = numpy.real(ncfile.variables['vr'][0, :, 0] *
+                    numpy.exp(1j*ncfile.variables['vr'][0, :, 1]))
+    print r.size
+    print vr.size
+
+    vt = numpy.real(ncfile.variables['vt'][0, :, 0] *
+                    numpy.exp(1j*ncfile.variables['vt'][0, :, 1]))
+
+    vz = numpy.real(ncfile.variables['vz'][0, :, 0] *
+                    numpy.exp(1j*ncfile.variables['vz'][0, :, 1]))
+
+    br = numpy.real(ncfile.variables['br'][0, :, 0] *
+                    numpy.exp(1j*ncfile.variables['br'][0, :, 1]))
+
+    bt = numpy.real(ncfile.variables['bt'][0, :, 0] *
+                    numpy.exp(1j*ncfile.variables['bt'][0, :, 1]))
+
+    bz = numpy.real(ncfile.variables['bz'][0, :, 0] *
+                    numpy.exp(1j*ncfile.variables['bz'][0, :, 1]))
+
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    
+    ax.plot(r, vr/3.0, label=r"$\phi_r\times1/3$")
+    ax.plot(r, vt, label=r"$\phi_\theta$")
+    ax.plot(r, vz*0.07, label=r"$\phi_z\times0.07$")
+    ax.plot(r, br, label=r"$\beta_r$")
+    ax.plot(r, bt*5, label=r"$\beta_\theta\times5$")
+    ax.plot(r, bz, label=r"$\beta_z$")
+    ax.set_xlim(ncfile.r1, ncfile.r2)
+    ax.set_xlabel(r"$r$ [cm]")
+    ax.legend(loc='lower right', ncol=2)
+
+    fig.savefig(filename, bbox_inches='tight', pad_inches=0.01)
+
+    ncfile.close()
