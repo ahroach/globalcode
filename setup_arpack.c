a6fce8099331346a4877878ec6901635eaf5fa5e
Austin Roach <ahroach@gmail.com>
2012-06-27 15:10:48 -0400

    Don't try to get sigma_r, sigma_i from inputfile if iterating to find them

--- setup_arpack.c
+++ setup_arpack.c
@@ -20,12 +20,19 @@ ARPACK_CONTROL *setup_arpack(char *input_file_name) {
     arpack_params->nummodes = 1;
   }
 
-  arpack_params->sigma = get_dparam("sigma_r", input_file_name);
-  imagpart = get_dparam("sigma_i", input_file_name);
-  arpack_params->sigma = arpack_params->sigma + I*imagpart;
+  arpack_params->iterate = get_iparam("iterate", input_file_name);
+
+  //We only need to grab sigma from the input file if we're not iterating.
+  if(arpack_params->iterate) {
+    arpack_params->sigma = 0;
+  } else {
+    arpack_params->sigma = get_dparam("sigma_r", input_file_name);
+    imagpart = get_dparam("sigma_i", input_file_name);
+    arpack_params->sigma = arpack_params->sigma + I*imagpart;
+  }
   arpack_params->tol = get_dparam("tol", input_file_name);
   arpack_params->maxiters = get_iparam("maxiters", input_file_name);
-  arpack_params->iterate = get_iparam("iterate", input_file_name);
+
 
   //Make sure the mode specified by "which" is valid. If not, just
   //default to "LM"
