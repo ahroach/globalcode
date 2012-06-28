936710db942694482d3134777b5446dd5e17a0c1
Austin Roach <ahroach@gmail.com>
2012-06-28 18:15:04 -0400

    Add plotting routines for the kscans
    Scans are a bunch of runs at fixed omega, but varying B and k_z, to plot
    the curves that I'm supposedly finding the interesting points of.

--- python/kcritscantools.py
+++ python/kcritscantools.py
@@ -81,6 +81,60 @@ def plot_quantities_const_omega(rule, omega1):
     ax1.set_ylabel(r"$k_z$ [1/cm]")
     ax2.set_ylabel(r"Re{$\gamma$} [1/s]")
 
+
+def grab_data_kscan(rule):
+    files = glob.glob(rule)
+
+    numfiles = len(files)
+
+    data =  numpy.ones(numfiles, dtype = [('kz', float), ('B', float),
+                                          ('gr', float)])
+
+    for i in range(0,numfiles):
+        data[i]['kz'] = float(re.split('B',
+                                       re.split('k', files[i])[1])[0])
+        data[i]['B'] = float(re.split('.nc',
+                                      re.split('B', files[i])[1])[0])
+
+        ncfile = netcdf.netcdf_file(files[i], 'r')
+        data[i]['gr'] = ncfile.variables['lambda'][0,0]
+        ncfile.close()
+
+    #Sort the array so things are nicer
+    data = numpy.sort(data, order=['B', 'kz'])
+    
+    return data
+
+def plot_kscan_curves(rule):
+    data = grab_data_kscan(rule)
+    
+    Bs = []
+
+    #Find the unique Bs.
+    for i in range (0, data.size):
+        if not(Bs.count(data[i]['B'])):
+            Bs.append(data[i]['B'])
+    
+    #Now for each B, find the indexes that correspond to it.
+    Bidx = []
+    for i in range(0, len(Bs)):
+        Bidx.append(numpy.equal(data[:]['B'],
+                                Bs[i]*numpy.ones(data.size)))
+    
+    #Now plot each of these.
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111)
+    for i in range(0, len(Bs)):
+        ax.plot(data[Bidx[i]]['kz'], data[Bidx[i]]['gr'],
+                '.-', label="B=%g" % Bs[i])
+    
+    ax.set_xlabel(r"$k_z$ [1/cm]")
+    ax.set_ylabel(r"Re[$\gamma$] [1/s]")
+    ax.autoscale(axis='y', tight=True)
+    ax.set_ylim(bottom=-5)
+    ax.set_xscale('log')
+    ax.legend(loc='best')
+
 def elsasser(omega, B):
     rho = 6.36 #Density in g/cm^3
     eta = 2.43e3 #Resistivity in cm^2/s
